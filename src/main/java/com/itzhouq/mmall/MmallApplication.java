2508ed0273625717895c3f6575f715499fa02fc5
itzhouq <itzhouq@163.com>
2020-07-15 21:18:44 +0800

    init

---
+++ src/main/java/com/itzhouq/mmall/MmallApplication.java
@@ -0,0 +1,13 @@
+package com.itzhouq.mmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmallApplication.class, args);
+    }
+
+}
